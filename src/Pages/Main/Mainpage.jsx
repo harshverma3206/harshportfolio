1683d212f57b035d74226cc4579f3d56a953db0f
Harsh <harshverma3206@gmail.com>
2026-04-21 08:18:41 +0530

    custom cursor

--- src/Pages/Main/Mainpage.jsx
+++ src/Pages/Main/Mainpage.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { gsap } from 'gsap'
+
 import Home from './Sections/Home'
 import About from './Sections/About'
 import Contact from './Sections/Contact'
@@ -7,8 +9,29 @@ import Projects from './Sections/Projects'
 import Navigation from '../../Components/Navigation/Navigation'
 
 const Mainpage = () => {
+
+  const customMouse = useRef()
+
+  const handler = (e) => {
+    console.log(e.clientX, e.clientY)
+
+    gsap.to(customMouse.current, {
+      x: e.clientX - 125,
+      y: e.clientY - 125,
+      duration: 0.5,
+    })
+  }
+
   return (
-    <div className='bg-gradient-to-r from-[#bfbfbf] to-[#ffffff] p-5! lg:px-10! sm:py-10! flex flex-col gap-25!'>
+    <div
+      onMouseMove={(e) => {
+        handler(e)
+      }}
+      className='bg-gradient-to-r from-[#bfbfbf] to-[#ffffff] p-5! lg:px-10! sm:py-10! flex flex-col gap-25! relative'>
+      <div
+        className='bg-[#c0c0c0] h-40 w-40 fixed rounded-full pointer-events-none'
+        ref={customMouse}
+      ></div>
       <Navigation />
       <Home />
       <About />
